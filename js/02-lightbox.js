1564cbbf0e878e59f32d7dee78747a081c75d61a
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-11-15 17:54:08 +0200

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  `
+    )
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery .gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
