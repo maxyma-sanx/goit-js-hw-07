45ff90c433a2145a0e37f63503f2d2c0a488af99
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-11-15 16:35:53 +0200

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `
+          <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+          <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"/>
+          </a>
+          </div>
+          `
+    )
+    .join("");
+}
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}">`
+  );
+
+  instance.show();
+  closeModalOnEscapeClick(instance);
+}
+
+function closeModalOnEscapeClick(instance) {
+  window.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+gallery.addEventListener("click", onGalleryClick);
